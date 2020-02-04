fb004ae1450e63716cb49accd148834ca4e99a76
Rachel Thompson <rthompson@turnitin.com>
2020-02-04 14:40:02 -0500

    configure with regular expessions

--- packages/remark-math/inline.js
+++ packages/remark-math/inline.js
@@ -4,78 +4,84 @@ function locator (value, fromIndex) {
 
 const ESCAPED_INLINE_MATH = /^\\\$/ // starts with \$
 
-/*
-const customDelimiters = {
-  math: {
-    left: '\\$\\$\\$',
-    right: '\\$\\$\\$',
-    matchInclude: ['\\\\\\$', '[^$]'],
-    tagName: 'div',
-    classNames: ['math', 'math-display']
-  },
-  inlineMath: {
-    left: '\\$\\$',
-    right: '\\$\\$',
-    matchInclude: ['\\\\\\$', '[^$]'],
-    tagName: 'span',
-    classNames: ['inlineMath', 'math-inline']
-  }
-} */
+const NODE_TYPES = {
+  TEXT: 'text',
+  MATH_DISPLAY: 'math',
+  MATH_INLINE: 'inlineMath'
+}
 
-const defaultDelimiters = {
-  math: {
-    left: '\\$\\$',
-    right: '\\$\\$',
-    matchInclude: ['\\\\\\$', '[^$]'],
-    tagName: 'span',
-    classNames: ['inlineMath']
+/*
+const customModes = {
+  mathDisplay: {
+    nodeType: NODE_TYPES.MATH_DISPLAY,
+    left: /\$\$\$/,
+    right: /\$\$\$/,
+    matchInclude: [/\\\$/, /[^$]/],
+    tagName: 'div',
+    getClassNames: function () { return ['math', 'math-display'] }
   },
-  inlineMath: {
-    left: '\\$',
-    right: '\\$',
-    matchInclude: ['\\\\\\$', '[^$]'],
+  mathInline: {
+    nodeType: NODE_TYPES.MATH_INLINE,
+    left: /\$\$/,
+    right: /\$\$/,
+    matchInclude: [/\\\$/, /[^$]/],
     tagName: 'span',
-    classNames: ['inlineMath']
+    getClassNames: function () { return ['inlineMath', 'math-inline'] }
+  }
+}
+*/
+
+const defaultModes = {
+  mathInlineDouble: {
+    nodeType: NODE_TYPES.MATH_INLINE,
+    left: /\$\$/,
+    right: /\$\$/,
+    matchInclude: [/\\\$/, /[^$]/],
+    tagName: 'span',
+    getClassNames: function (opts) {
+      return opts && opts.inlineMathDouble ? ['inlineMath', 'inlineMathDouble'] : ['inlineMath']
+    }
+  },
+  mathInline: {
+    nodeType: NODE_TYPES.MATH_INLINE,
+    left: /\$/,
+    right: /\$/,
+    matchInclude: [/\\\$/, /[^$]/],
+    tagName: 'span',
+    getClassNames: function () { return ['inlineMath'] }
   }
 }
 
-function buildMatchers (delimiters) {
-  return Object.keys(delimiters).reduce(function (accum, category) {
-    const categoryInfo = delimiters[category]
-    const left = categoryInfo.left
-    const right = categoryInfo.right
-    const matchInclude = categoryInfo.matchInclude
+function buildMatchers (modes) {
+  return Object.keys(modes).reduce(function (accum, modeName) {
+    const mode = modes[modeName]
+    const left = mode.left.source
+    const right = mode.right.source
+    const matchInclude = mode.matchInclude.map(function (s) { return s.source })
     const capture = '((?:' + matchInclude.join('|') + ')+)'
-    accum[category] = new RegExp('^' + left + capture + right)
+    accum[modeName] = new RegExp('^' + left + capture + right)
     return accum
   }, {})
 }
 
 function findMatch (matchers, value) {
-  let category = null
+  let modeName = null
   let match = null
-  for (category in matchers) {
-    const matcher = matchers[category]
-    if (!matcher) {
-      console.log('category', category)
-      console.log('matchers', matchers)
-    }
+  for (modeName in matchers) {
+    const matcher = matchers[modeName]
     match = matcher.exec(value)
     if (match) {
       break
     }
   }
 
-  return match && { category, match, fullMatch: match[0], content: match[1].trim() }
+  return match && { modeName, match, fullMatch: match[0], content: match[1].trim() }
 }
 
 module.exports = function inlinePlugin (opts) {
-  const delimiters = (opts && opts.delimiters) || defaultDelimiters
-  if (opts && opts.inlineMathDouble && delimiters.math) {
-    delimiters.math.classNames.push('inlineMathDouble')
-  }
+  const modes = (opts && opts.modes) || defaultModes
 
-  const matchers = buildMatchers(delimiters)
+  const matchers = buildMatchers(modes)
 
   function inlineTokenizer (eat, value, silent) {
     const matchData = findMatch(matchers, value)
@@ -87,14 +93,14 @@ module.exports = function inlinePlugin (opts) {
         return true
       }
       return eat(escaped[0])({
-        type: 'text',
+        type: NODE_TYPES.TEXT,
         value: '$'
       })
     }
 
     if (value.slice(-2) === '\\$') { // ends with \$
       return eat(value)({
-        type: 'text',
+        type: NODE_TYPES.TEXT,
         value: value.slice(0, -2) + '$'
       })
     }
@@ -113,22 +119,23 @@ module.exports = function inlinePlugin (opts) {
     if (endingDollarInBackticks) {
       const toEat = value.slice(0, value.indexOf('`'))
       return eat(toEat)({
-        type: 'text',
+        type: NODE_TYPES.TEXT,
         value: toEat
       })
     }
 
+    const mode = modes[matchData.modeName]
     return eat(fullMatch)({
-      type: 'inlineMath',
+      type: mode.nodeType,
       value: matchData.content,
       data: {
-        hName: delimiters[matchData.category].tagName,
+        hName: mode.tagName,
         hProperties: {
-          className: delimiters[matchData.category].classNames.join(' ')
+          className: mode.getClassNames(opts).join(' ')
         },
         hChildren: [
           {
-            type: 'text',
+            type: NODE_TYPES.TEXT,
             value: matchData.content
           }
         ]
@@ -142,15 +149,15 @@ module.exports = function inlinePlugin (opts) {
   // Inject inlineTokenizer
   const inlineTokenizers = Parser.prototype.inlineTokenizers
   const inlineMethods = Parser.prototype.inlineMethods
-  inlineTokenizers.math = inlineTokenizer
-  inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'math')
+  inlineTokenizers[NODE_TYPES.MATH_DISPLAY] = inlineTokenizer
+  inlineMethods.splice(inlineMethods.indexOf(NODE_TYPES.TEXT), 0, NODE_TYPES.MATH_DISPLAY)
 
   const Compiler = this.Compiler
 
   // Stringify for math inline
   if (Compiler != null) {
     const visitors = Compiler.prototype.visitors
-    visitors.inlineMath = function (node) {
+    visitors[NODE_TYPES.MATH_INLINE] = function (node) {
       return '$' + node.value + '$'
     }
   }
